3d990ea43df6dd4b0fb0cf0ea7f9a62985c375f8
yuri <yuri@example.com>
2023-07-12 22:13:45 +0900

    14. 記事タイトルやタグ取得用のメタデータ取得関数を作ってみよう

--- lib/notionAPI.ts
+++ lib/notionAPI.ts
@@ -3,13 +3,24 @@ import { Client } from "@notionhq/client";
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
-// console.log(process.env.NOTION_TOKEN);
-// console.log(process.env.NOTION_cDATABASE_ID);
+
 export const getAllPosts = async () => {
   const posts = await notion.databases.query({
     database_id: process.env.NOTION_DATABASE_ID || "",
     page_size: 100,
   });
   const allPosts = posts.results;
-  return allPosts;
+  return allPosts.map((post) => {
+    return getPageMetaData(post);
+  });
+};
+
+const getPageMetaData = (post) => {
+  return {
+    id: post.id,
+    title: post.properties.Name.title[0].plain_text,
+    description: post.properties.Description.rich_text[0].plain_text,
+    date: post.properties.Date.date.start,
+    slug: post.properties.Slug.rich_text[0].plain_text,
+  };
 };
