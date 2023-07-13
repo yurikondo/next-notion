d84f48daa6035daba5d416656cc8e8a635cbc7d7
yuri <yuri@example.com>
2023-07-13 18:45:00 +0900

    17. ブログ用にレイアウトを調整してみよう

--- components/Post/SinglePost.tsx
+++ components/Post/SinglePost.tsx
@@ -1,16 +1,35 @@
+import Link from "next/link";
 import React from "react";
 
 type Props = {
   title: string;
   description: string;
   date: string;
-  tag: string;
+  tags: string[];
   slug: string;
 };
 
 const SinglePost = (props: Props) => {
-  const { title, description, date, tag, slug } = props;
-  return <div>{title}</div>;
+  const { title, description, date, tags, slug } = props;
+  return (
+    <Link href={`/posts/${slug}`}>
+      <section className="lg:w-1/2 bg-sky-900 mb-8 mx-auto rounded-md p-5 shadow-2xl hover:shadow-none hover:translate-y-1 transition-all duration-300">
+        <div className="flex items-center gap-3">
+          <h2 className="text-gray-100 text-2xl font-medium mb-2">{title}</h2>
+          <div className="text-gray-100">{date}</div>
+          {tags.map((tag, index) => (
+            <span
+              key={index}
+              className="text-white bg-gray-500 rounded-xl px-2 pb-1 font-medium"
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+        <p className="text-gray-100">{description}</p>
+      </section>
+    </Link>
+  );
 };
 
 export default SinglePost;
--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,7 @@ export default function Home({ allPosts }) {
                 title={post.title}
                 description={post.description}
                 date={post.date}
-                tag={post.tag}
+                tags={post.tags}
                 slug={post.slug}
               />
             </div>
