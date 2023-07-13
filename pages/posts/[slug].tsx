30772d21a1504fe7d8cd8f7016427f1222f1d3f7
yuri <yuri@example.com>
2023-07-13 19:28:31 +0900

    20. 動的ルーディングでNotion記事詳細ページを作成してみよう

---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Post = () => {
+  return (
+    <section className="container lg:px-2 px-5 h-screen lg:w-2/5 mx-auto mt-20">
+      <h2 className="w-full text-2xl font-medium">詳細記事ページ</h2>
+      <div className="border-b-2 w-1/3 mt-1 border-sky-900"></div>
+      <span className="text-gray-500">2023/07/10</span>
+      <br />
+      <p className="text-white bg-sky-900 rounded-xl font-medium mt-2 px-2 inline-block">
+        Next.js
+      </p>
+      <div className="mt-10 font-medium">sぢ音kさんvlkdさんvかsんvsg</div>
+    </section>
+  );
+};
+
+export default Post;
